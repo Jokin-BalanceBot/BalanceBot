e4610e68e0a4a28858a8a696f59ac31bd4867be6
Jokin-BalanceBot <46402465+Jokin-BalanceBot@users.noreply.github.com>
2019-01-06 22:04:19 +0100

    Add files via upload

---
+++ Lektion 2/config.php
@@ -0,0 +1,18 @@
+<?php
+
+// Abschnitt config.php 1: Zugangsdaten für Datenbankverbindung
+$__db="balancebot";
+$__db_server="localhost";
+$__db_user="balancebot";
+$__db_passwort="BalanceBotPasswort";
+
+// Abschnitt config.php 2: 
+// (Platzhalter)
+
+// Abschnitt config.php 3: 
+// (Platzhalter)
+
+// Abschnitt config.php 4: 
+// (Platzhalter)
+
+?>
