759ceafca70a450cfe801cd5fe17dbf98f01f70f
Jokin-BalanceBot <46402465+Jokin-BalanceBot@users.noreply.github.com>
2019-01-16 20:53:26 +0100

    Add files via upload

---
+++ Lektion 7/csv.php
@@ -0,0 +1,103 @@
+<?php
+/***
+*
+* Via phpmyadmin Tabelle, Priämarschlüssel, auto_increment anlegen
+
+CREATE TABLE `tblBalances` (
+  `id` int(11) NOT NULL,
+  `coin_name` varchar(10) NOT NULL,
+  `pair_name` varchar(10) NOT NULL,
+  `date_created` datetime NOT NULL,
+  `current_balance` double NOT NULL,
+  `current_price` double NOT NULL,
+  `target_percentage` double NOT NULL,
+  `virtual_percentage` double NOT NULL,
+  `virtual_balance` double NOT NULL
+
+);
+
+ALTER TABLE `tblBalances`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `tblBalances`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+*/
+
+require_once 'config.php'; // Config-Datei einbinden
+
+// Abschnitt show_csv.php 1:
+// Datenbankverbindung aufbauen und einen DB-Handler erzeugen
+$mysqli = new mysqli($__db_server, $__db_user, $__db_passwort, $__db);
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    exit(); // Skript beenden, wenn keine DB-Verbindung zustande kommt.
+}
+
+// Abschnitt show_csv.php 2:
+// Daten aus der Datenbank auslesen
+$sql_query="SELECT * FROM `tblBalances` ";
+$result_tblBalances = $mysqli->query($sql_query);
+
+
+// Abschnitt show_csv.php 3:
+// Daten aufbereiten
+// Alle Einträge einer Zeile sollen einem Zeitpunkt zugeordnet werden
+while($entry_tblBalances = $result_tblBalances->fetch_array(MYSQLI_BOTH)) {
+	$array_csv[$entry_tblBalances['date_created']][$entry_tblBalances['coin_name']] = $entry_tblBalances;
+}
+
+// Abschnitt show_csv.php 4:
+// Daten ausgeben
+// Hier geben wir nicht alles aus, was in der Datenbank steht sondern nur das, 
+// was derzeit in der Config.php definiert ist.
+// Nach Anpassung der Coins in der config.php führt das zu Auswertungsfehler, 
+// die aufgrund der Einfachheit in Kauf genommen werden.
+
+// Abschnitt show_csv.php 4.1:
+// Titel der CSV-Datei ausgeben
+
+// Abschnitt show_csv.php 4.1.1:
+echo "Datum;";
+echo "virtual ".$__balanceBot_basecurrency['name']." in %;";
+echo "virtual ".$__balanceBot_basecurrency['name']." in Stück;";
+echo "current ".$__balanceBot_basecurrency['name']." in Stück;";
+
+// Abschnitt show_csv.php 4.1.2:
+foreach ($__balanceBot_coins as $key => $array_coin){
+	echo "virtual ".$__balanceBot_coins[$key]['name']." in %;";
+	echo "virtual ".$__balanceBot_coins[$key]['name']." in Stück;";
+	echo "virtual ".$__balanceBot_coins[$key]['name']." in ".$__balanceBot_basecurrency['name'].";";
+	echo "current ".$__balanceBot_coins[$key]['name']." in Stück;";
+	echo "current ".$__balanceBot_coins[$key]['name']." in ".$__balanceBot_basecurrency['name'].";";
+}
+echo "<br>";
+
+// Abschnitt show_csv.php 4.2:
+// Daten der CSV-Datei ausgeben
+foreach ($array_csv as $key => $entry_csv){
+
+	// Abschnitt show_csv.php 4.2.1:
+	echo $key.";";
+	echo $entry_csv[$__balanceBot_basecurrency['name']]['virtual_percentage'].";";
+	echo $entry_csv[$__balanceBot_basecurrency['name']]['virtual_balance'].";";
+	echo $entry_csv[$__balanceBot_basecurrency['name']]['current_balance'].";";
+
+	// Abschnitt show_csv.php 4.2.2:
+	foreach ($__balanceBot_coins as $key => $array_coin){
+		echo $entry_csv[$__balanceBot_coins[$key]['name']]['virtual_percentage'].";";
+		echo $entry_csv[$__balanceBot_coins[$key]['name']]['virtual_balance'].";";
+		echo $entry_csv[$__balanceBot_coins[$key]['name']]['virtual_balance'] * $entry_csv[$__balanceBot_coins[$key]['name']]['current_price'].";";
+		echo $entry_csv[$__balanceBot_coins[$key]['name']]['current_balance'].";";
+		echo $entry_csv[$__balanceBot_coins[$key]['name']]['current_balance'] * $entry_csv[$__balanceBot_coins[$key]['name']]['current_price'].";";
+	}
+	echo "<br>";
+}
+
+
+
+
+
+?>
+
+
---
+++ Lektion 7/show_messages.php
@@ -0,0 +1,56 @@
+<?php
+
+// Abschnitt show_messages.php 1: Datei mit den Konfigurationsparametern einlesen
+require_once("config.php");
+
+// Abschnitt show_messages.php 2: Datenbankverbindung aufbauen und einen DB-Handler erzeugen
+$mysqli = new mysqli($__db_server, $__db_user, $__db_passwort, $__db);
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    exit(); // Skript beenden, wenn keine DB-Verbindung zustande kommt.
+}
+
+echo "DB-Verbindung hat offenbar geklappt<br>";
+
+
+// Abschnitt show_messages.php 3:
+// Die Linkliste zu Beginn der Seite einbinden
+// mit <?php ... lässt sich PHP-Code innerhalb von HTML-Code einfügen.
+?>
+<a href="<?php echo $_SERVER["PHP_SELF"] ?>?limit=600">alle Messages</a> |
+<a href="<?php echo $_SERVER["PHP_SELF"] ?>?limit=600&search=Creat">Create</a> |
+<a href="<?php echo $_SERVER["PHP_SELF"] ?>?limit=600&search=Order">Order</a> |
+<a href="<?php echo $_SERVER["PHP_SELF"] ?>?limit=600&search=Status">Status</a> |
+<a href="<?php echo $_SERVER["PHP_SELF"] ?>?limit=600&search=Error">Error</a> |
+<a href="<?php echo $_SERVER["PHP_SELF"] ?>?limit=600&search=Fehler">Fehler</a> |
+<a href="<?php echo $_SERVER["PHP_SELF"] ?>?limit=600&search=csv">csv</a> |
+<hr>
+<?php
+
+
+// Abschnitt show_messages.php 4:
+// Daten, die mit der URL übergeben werden, nehmen wir hier in das Skript auf
+$limit = $_GET["limit"];
+$search = $_GET["search"];
+
+// falls keine Werte übermittelt worden sind, setzen wir hier Standardwerte
+if (!$limit or !is_numeric($limit)) $limit = 600; // falls "limit" nicht numerisch ist, dann wird das hier abgefangen.
+if (!$search) $search = ''; // "$search" kann beliebige Suchwerte beinhalten - individuelle Auswertungen werden so möglich
+
+
+// Abschnitt show_messages.php 5:
+// Nun wollen wir auslesen was in der Datenbank drin steht.
+// Dazu wollen wir die DB abfragen und die Ergebnisse sollen nach der ID absteigend sortiert werden (Datum ginge auch)
+$sql_query="SELECT * FROM `tblMessages` WHERE `message` like '%".$mysqli->real_escape_string($search)."%' ORDER BY  `id` DESC LIMIT 0, ".$mysqli->real_escape_string($limit)." ";
+
+$result_tblMessages = $mysqli->query($sql_query);
+
+// in einer while-Schleife durchlaufen wir jeden Datensatz, den wir von der Datenbank zurück erhalten.
+while($entry_tblMessages = $result_tblMessages->fetch_array(MYSQLI_BOTH)) {
+	// ... und geben diesen einfach über einen echo-Befehl im Browser aus
+    echo " ".$entry_tblMessages['date_created'].": ".$entry_tblMessages['message']." <br>";
+}
+
+
+?>
